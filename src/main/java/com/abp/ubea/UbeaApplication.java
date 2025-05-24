25b43eef1d76473b4caedbe70a2259bd0bd353e6
Pedro Murilo <pedromarmindo@gmail.com>
2025-05-24 15:15:36 -0300

    Initial commit

---
+++ src/main/java/com/abp/ubea/UbeaApplication.java
@@ -0,0 +1,13 @@
+package com.abp.ubea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abp/ubea/UbeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abp.ubea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
