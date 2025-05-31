44e375cf3887036df648bbc749e243b6d78b27ec
Pedro Murilo <pedromarmindo@gmail.com>
2025-05-31 15:51:18 -0300

    feat: add create, update and delete endpoints for User

--- src/main/java/com/abp/ubea/controllers/UserController.java
+++ src/main/java/com/abp/ubea/controllers/UserController.java
@@ -1,14 +1,10 @@
 package com.abp.ubea.controllers;
 
 import com.abp.ubea.dtos.UserDTO;
-import com.abp.ubea.entities.UserEntity;
 import com.abp.ubea.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +25,23 @@ public class UserController {
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         UserDTO user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO dto) {
+        dto = userService.create(dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDTO> update(@RequestBody UserDTO dto) {
+        dto = userService.update(dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/abp/ubea/services/UserService.java
+++ src/main/java/com/abp/ubea/services/UserService.java
@@ -1,6 +1,7 @@
 package com.abp.ubea.services;
 
 import com.abp.ubea.dtos.UserDTO;
+import com.abp.ubea.entities.UserEntity;
 import com.abp.ubea.exceptions.ResourceNotFoundException;
 import com.abp.ubea.mappers.UserMapper;
 import com.abp.ubea.repositories.UserRepository;
@@ -30,5 +31,29 @@ public class UserService {
         return userRepository.findById(id)
                 .map(userMapper::convertEntityToDTO)
                 .orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public UserDTO create(UserDTO dto){
+        UserEntity user = userMapper.convertDTOToEntity(dto);
+        return userMapper.convertEntityToDTO(userRepository.save(user));
+    }
+
+    public UserDTO update(UserDTO dto) {
+        UserEntity user = userRepository.findById(dto.getId())
+                .orElseThrow(() -> new ResourceNotFoundException(dto.getId()));
+
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setRole(dto.getRole());
+        user.setEnabled(dto.getEnabled());
+        user.setPhone(dto.getPhone());
+
+        return userMapper.convertEntityToDTO(userRepository.save(user));
+    }
+
+    public void delete(Integer id) {
+        if (!userRepository.existsById(id)) throw new ResourceNotFoundException(id);
+        userRepository.deleteById(id);
     }
 }
